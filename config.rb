4a635df6ddccd3853878281eeb8093200cc6b17b
Marc-Andre Lafortune <github@marc-andre.ca>
2013-04-24 16:38:58 -0400

    asset hash + cache

--- config.rb
+++ config.rb
@@ -100,8 +100,9 @@ configure :build do
 
   # Minify Javascript on build
   activate :minify_javascript
+  activate :asset_hash, exts: %w[.js .css]
   # Enable cache buster
-  # activate :cache_buster
+  activate :cache_buster
 
   # Use relative URLs
   # activate :relative_assets

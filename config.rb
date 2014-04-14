132f3f052be7ef750050266c0abb22777ed301be
Marc-Andre Lafortune <github@marc-andre.ca>
2014-04-14 15:23:55 -0400

    Tweak config & links

--- config.rb
+++ config.rb
@@ -73,7 +73,7 @@ helpers do
     m = m[:movie] if m.is_a?(Hash)
     return '' unless m
     @movies ||= data[:movies]
-    link_to m, "www.imdb.com/title/#{@movies.fetch(m)}/"
+    link_to m, "http://www.imdb.com/title/#{@movies.fetch(m)}/"
   end
 end
 
@@ -86,16 +86,16 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash, exts: %w[.js .css]
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"

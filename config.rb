23e70ac0537b172708fa1d77551bcb613aa2b1af
Marc-Andre Lafortune <github@marc-andre.ca>
2014-04-14 15:17:40 -0400

    Update middleman

--- config.rb
+++ config.rb
@@ -2,32 +2,13 @@
 # Compass
 ###
 
-# Susy grids in Compass
-# First: gem install compass-susy-plugin
-# require 'susy'
-
 # Change Compass configuration
 # compass_config do |config|
 #   config.output_style = :compact
 # end
 
 ###
-# Haml
-###
-
-# CodeRay syntax highlighting in Haml
-# First: gem install haml-coderay
-# require 'haml-coderay'
-
-# CoffeeScript filters in Haml
-# First: gem install coffee-filter
-# require 'coffee-filter'
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-###
-# Page command
+# Page options, layouts, aliases and proxies
 ###
 
 # Per-page layout changes:
@@ -43,14 +24,21 @@
 #   page "/admin/*"
 # end
 
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
 ###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
+# end
 
 # Methods defined in the helpers block are available in templates
 helpers do
@@ -89,35 +77,26 @@ helpers do
   end
 end
 
-# Change the CSS directory
-# set :css_dir, "alternative_css_directory"
+set :css_dir, 'stylesheets'
 
-# Change the JS directory
-# set :js_dir, "alternative_js_directory"
+set :js_dir, 'javascripts'
 
-# Change the images directory
-# set :images_dir, "alternative_image_directory"
+set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  set :build_dir, "."
   # For example, change the Compass output style for deployment
-  activate :minify_css
+  # activate :minify_css
 
   # Minify Javascript on build
-  activate :minify_javascript
-  activate :asset_hash, exts: %w[.js .css]
+  # activate :minify_javascript
+
   # Enable cache buster
-  activate :cache_buster
+  # activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
 
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
   # Or use a different image path
-  # set :http_path, "/Content/images/"
+  # set :http_prefix, "/Content/images/"
 end
